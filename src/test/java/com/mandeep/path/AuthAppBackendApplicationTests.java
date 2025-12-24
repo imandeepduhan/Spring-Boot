bed971c330b73ab0a674826ebbe45339ba634f08
Mandeep <imandeepduhan@gmail.com>
2025-12-24 13:29:39 +0530

    Initial commit

---
+++ src/test/java/com/mandeep/path/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandeep.path;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
