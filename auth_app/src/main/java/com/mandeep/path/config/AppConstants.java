8c7a7ea76c7fed5dc1dbef5b5207486e173a9b4e
Mandeep <imandeepduhan@gmail.com>
2026-01-11 20:47:43 +0530

    Login with Google

---
+++ auth_app/src/main/java/com/mandeep/path/config/AppConstants.java
@@ -0,0 +1,10 @@
+package com.mandeep.path.config;
+
+public class AppConstants {
+    public static final String[] AUTH_PUBLIC_URLS = {
+            "/api/v1/auth/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**"
+    };
+}
--- auth_app/src/main/java/com/mandeep/path/config/SecurityConfig.java
+++ auth_app/src/main/java/com/mandeep/path/config/SecurityConfig.java
@@ -3,88 +3,81 @@ package com.mandeep.path.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mandeep.path.dtos.ApiError;
 import com.mandeep.path.security.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
-
+import io.jsonwebtoken.lang.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
 import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private AuthenticationSuccessHandler successHandler;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationSuccessHandler successHandler) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.successHandler = successHandler;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(AbstractHttpConfigurer::disable)
+        http.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .sessionManagement(sm ->
-                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers(
-                                        "/api/v1/auth/register",
-                                        "/api/v1/auth/login",
-                                        "/api/v1/auth/refresh",
-                                        "/api/v1/auth/logout"
-                                ).permitAll()
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .requestMatchers(AppConstants.AUTH_PUBLIC_URLS).permitAll()
                                 .anyRequest().authenticated()
                 )
-                .exceptionHandling(ex ->
-                        ex.authenticationEntryPoint((request, response, e) -> {
-
-                            response.setStatus(401);
-                            response.setContentType("application/json");
-
-                            String message = e.getMessage();
-
-                            String error = (String) request.getAttribute("error");
-                            if (error != null) {
-                                message = error;
-                            }
-
-                            /*
-                            Map<String, Object> errorMap = Map.of(
-                                    "message", message,
-                                    "statusCode", "404"
-                            );
-                            new ObjectMapper().writeValue(response.getWriter(), errorMap);
-                            */
-
-                            var apiError = ApiError.of(
-                                    HttpStatus.UNAUTHORIZED.value(),
-                                    "Unauthorized Access",
-                                    message,
-                                    request.getRequestURI(),true
-                            );
-
-                            var objectMapper = new ObjectMapper();
-                            response.getWriter().write(
-                                    objectMapper.writeValueAsString(apiError)
-                            );
-                        })
+                .oauth2Login(oauth2 ->
+                        oauth2.successHandler(successHandler)
+                                .failureHandler(null)
                 )
+                .logout(AbstractHttpConfigurer::disable)
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, e) -> {
+                    //error message
+//                    e.printStackTrace();
+                    response.setStatus(401);
+                    response.setContentType("application/json");
+                    String message = e.getMessage();
+                    String error = (String) request.getAttribute("error");
+                    if (error != null) {
+                        message = error;
+                    }
+//                    Map<String, Object> errorMap = Map.of("message", message, "statusCode",404);
+                    var apiError = ApiError.of(HttpStatus.UNAUTHORIZED.value(), "Unauthorized Access", message, request.getRequestURI(), true);
+                    var objectMapper = new ObjectMapper();
+                    response.getWriter().write(objectMapper.writeValueAsString(apiError));
+                }))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+
         return http.build();
+
     }
 
     @Bean
@@ -93,9 +86,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration
-    ) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
+
 }
